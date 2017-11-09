577f1996acb6b9a3f138f8f2c0a0ff53cafbfbe6
Cammy <cameron.lamberth.2015@uni.strath.ac.uk>
2017-11-09 19:27:56 +0000

    Added a constructor, buy method, sell method and net gain method.

--- src/model/Stock.java
+++ src/model/Stock.java
@@ -3,10 +3,33 @@ package model;
 public class Stock {
 
     private String ticker;
-    private String name;
+    private String name; //no way of getting the name from provided classes
     private double sharePrice;
-    private double shares;
-    private double holdingValue;
-    private double boughtFor;
+    private double amount;
+    private double totalCost;
+    private double totalValueSold;
+
+    private Stock(String ticker, double sharePrice, double amount) {
+        this.ticker = ticker;
+        this.sharePrice = sharePrice;
+        this.amount = amount;
+        totalCost = sharePrice*amount;
+        totalValueSold = 0;
+    }
+
+    private void buy(double amount){
+        this.amount += amount;
+        totalCost += amount*sharePrice; //Ensure sharePrice is price when bought
+    }
+
+    private void sell(double amount){
+        this.amount -= amount;
+        totalValueSold += amount*sharePrice; //Ensure sharePrice is price when sold
+    }
+
+    //Returns net gain as a percentage
+    private double netGainPercentage(){
+        return ((sharePrice*amount+totalValueSold)/totalCost)*100;
+    }
 
 }
