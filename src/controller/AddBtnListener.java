1382645760701fff03427b58d948d73ce8963684
Lauren Higgins CS2015 <lauren.higgins.2015@uni.strath.ac.uk>
2017-11-25 21:12:58 +0000

    Finding components from a particular tab

--- src/controller/AddBtnListener.java
+++ src/controller/AddBtnListener.java
@@ -17,10 +17,43 @@ public class AddBtnListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        JPanel allPan = null;
+
+        //Retrieve first panel from tab
         Component[] components = tabbedPane.getComponents();
         for(int i = 0; i < components.length; i++)
         {
-            System.out.println(components[i]);
+            if (components[i] instanceof JPanel)
+            {
+                allPan = (JPanel) components[i];
+                System.out.println("jpanel found!");
+            }
         }
+
+        //Retrieve internal panels?
+        JPanel[] internalPanels = new JPanel[3];
+        int j = 0;
+        Component[] internalComps = allPan.getComponents();
+        for(int i = 0; i < internalComps.length; i++)
+        {
+            if (internalComps[i] instanceof JPanel)
+            {
+                internalPanels[j] = (JPanel) internalComps[i];
+                j++;
+            }
+        }
+
+        //Components of the internal panels
+        for(int i = 0; i < internalPanels.length; i++)
+        {
+            Component[] panComps = internalPanels[i].getComponents();
+            for(int k = 0; k < panComps.length; k++)
+            {
+                System.out.println(panComps[k]);
+            }
+            System.out.println("-----------------");
+        }
+
+
     }
 }
