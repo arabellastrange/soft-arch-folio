81d956a2408b008eacd6f454b4597426cce8e988
Lauren Higgins CS2015 <lauren.higgins.2015@uni.strath.ac.uk>
2017-11-25 21:43:41 +0000

    Add finds particular table when pressed

--- src/controller/AddBtnListener.java
+++ src/controller/AddBtnListener.java
@@ -43,16 +43,39 @@ public class AddBtnListener implements ActionListener {
             }
         }
 
-        //Components of the internal panels
-        for(int i = 0; i < internalPanels.length; i++)
+        JPanel inputPan = internalPanels[0];
+        JPanel tablePan = internalPanels[1];
+
+        //TextFields of the input panel
+        Component[] inputComps = inputPan.getComponents();
+        JTextField[] textFields = new JTextField[3];
+        JFormattedTextField noOfShares = null;
+        int a = 0;
+        for(int i = 0; i < inputComps.length; i++)
         {
-            Component[] panComps = internalPanels[i].getComponents();
-            for(int k = 0; k < panComps.length; k++)
+            if(inputComps[i] instanceof JTextField)
             {
-                System.out.println(panComps[k]);
+                textFields[a] = (JTextField) inputComps[i];
+                a++;
             }
-            System.out.println("-----------------");
         }
+
+        JTextField tickField = textFields[0];
+        JTextField nameField = textFields[1];
+        JTextField numField = textFields[2];
+
+        //JTable from table panel
+        Component[] tpComps = tablePan.getComponents();
+        JTable thisTable = null;
+        for (int i = 0; i < tpComps.length; i++)
+        {
+            if (tpComps[i] instanceof JTable)
+            {
+                System.out.println("JTABLE FOUND");
+                thisTable = (JTable) tpComps[i];
+            }
+        }
+        System.out.println(tpComps.length);
 
 
     }
