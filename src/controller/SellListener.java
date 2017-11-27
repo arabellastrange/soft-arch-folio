d18927410d2eaef3a8e131625470c7d241dc5662
wyb15135 <chadha.degachi.2015@uni.strath.ac.uk>
2017-11-27 12:55:21 +0000

    input buy and sell

--- src/controller/SellListener.java
+++ src/controller/SellListener.java
@@ -1,11 +1,12 @@
 package controller;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SellListener implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        String sellShares = JOptionPane.showInputDialog("Enter the amount you would like to sell");
     }
 }
